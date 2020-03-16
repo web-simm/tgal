21c870df2cca55af4671cb9abb994583178e082c
web-simm <pedro.ventura@web-simm.com>
2020-03-16 18:25:33 +0000

    Função add

--- calc/views.py
+++ calc/views.py
@@ -5,3 +5,13 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html',{'name':'Pedro Ventura'})
+
+
+def add(request):
+
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res = val1 + val2
+
+
+    return render(request, 'result.html',{'result':res})
