66850ee67896e8f6f58f3562dba9996ef70a2b8f
web-simm <pedro.ventura@web-simm.com>
2020-03-16 20:19:48 +0000

    Passagem de argumentos

--- galeria/views.py
+++ galeria/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def galeria(request):
-    return render(request, 'geral.html')
+    return render(request, 'geral.html', {'price': 700})
