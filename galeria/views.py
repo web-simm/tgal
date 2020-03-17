7e258bdf3e73558a46899b7e87de48ecf0b53eea
web-simm <pedro.ventura@web-simm.com>
2020-03-17 09:57:32 +0000

    Carregamento de imagens para a galeria

--- galeria/views.py
+++ galeria/views.py
@@ -8,8 +8,9 @@ def galeria(request):
     dest1 = Galeria()
     dest1.name = 'Sol'
     dest1.des = 'O brilho que faz...'
+    dest1.img = ''
     dest1.price = 25
-    
+
 
 
     return render(request, 'geral.html', {'dest1': dest1})
