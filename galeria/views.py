e680e6f47b59ed89dc215b1ebfb538d1f70a6cc7
web-simm <pedro.ventura@web-simm.com>
2020-03-17 10:27:46 +0000

    Overwrite imagens com collectstatics

--- galeria/views.py
+++ galeria/views.py
@@ -8,9 +8,21 @@ def galeria(request):
     dest1 = Galeria()
     dest1.name = 'Sol'
     dest1.des = 'O brilho que faz...'
-    dest1.img = ''
+    dest1.img = '814.jpg'
     dest1.price = 25
 
+    dest2 = Galeria()
+    dest2.name = 'Lua'
+    dest2.des = 'O brilho que faz...'
+    dest2.img = '/static/815.jpg'
+    dest2.price = 25
 
+    dest3 = Galeria()
+    dest3.name = 'Chuva'
+    dest3.des = 'O brilho que faz...'
+    dest3.img = '816.jpg'
+    dest3.price = 25
 
-    return render(request, 'geral.html', {'dest1': dest1})
+    dests = [dest1, dest2, dest3]
+
+    return render(request, 'geral.html', {'dests': dests})
