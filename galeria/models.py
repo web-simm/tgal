e4ef85d8d9f9d0769d807b1f82f0200b2de6aba1
web-simm <pedro.ventura@web-simm.com>
2020-03-18 09:50:09 +0000

    Criação na db galeria "tgal" -tb "galeria_galeria"

--- galeria/models.py
+++ galeria/models.py
@@ -2,9 +2,22 @@ from django.db import models
 
 # Create your models here.
 
-class Galeria:
-    id : int
-    name : str
-    img : str
-    des : str
-    price : int
+class Galeria(models.Model):
+    name = models.CharField(max_length=100)
+    img = models.ImageField(upload_to='img')
+    des = models.TextField()
+    price = models.IntegerField()
+
+# Criação na db galeria "tgal" a tabela "galeria_galeria"
+
+# python manage.py makemigrations
+
+# python manage.py sqlmigrate galeria 0001
+
+# -- Create model Galeria
+# --
+# CREATE TABLE "galeria_galeria" ("id" serial NOT NULL PRIMARY KEY, "name" varchar(100) NOT NULL, "img" varchar(100) NOT NULL, "des" text NOT NULL, "price" integer NOT NULL);
+# COMMIT;
+
+# python manage.py migrate     
+
--- galeria/urls.py
+++ galeria/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .models import Galeria
 
 urlpatterns = [
     path('',views.galeria, name='geral'),
